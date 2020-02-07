e3c9e2e2df858b4a5f13ccd30a5d619902d64d94
Martin Wessel <mwessel@herzing.edu>
2020-02-07 11:29:35 -0600

    Switch to using Promise methods

--- js/block_appointlet.js
+++ js/block_appointlet.js
@@ -11,29 +11,47 @@
 
   $( document ).ready( function() {
     // Code that should run only on page load
-    $.getJSON( '/webform/lookup_cookie', function( data ) {
 
-     var  bookable = drupalSettings.block_appointlet.campus_bookable[data.campus];
+    // get field data and load appointlet javascript
+    $.getJSON( '/webform/lookup_cookie' )
+      .done(function( data ) {
 
-      $('.appointlet-button').each( function() {
+        // bail if no results
+        if( $.isEmptyObject (data ) ) { return; }
 
-        $(this).attr( {
-          'data-appointlet-bookable'          : bookable,
-          'data-appointlet-email'             : data.email,
-          'data-appointlet-field-homephone'   : data.phone,
-          'data-appointlet-field-firstname'   : data.first_name,
-          'data-appointlet-field-lastname'    : data.last_name,
-          'data-appointlet-field-postalcode'  : data.zip,
-          'data-appointlet-field-program'     : data.program,
-          'data-appointlet-field-campuscode'  : data.campus,
+        // calculate bookable
+        var  bookable = drupalSettings.block_appointlet.campus_bookable[data.campus];
+
+        // set attributes
+        $('.appointlet-button').each( function() {
+
+          $(this).attr( {
+            'data-appointlet-bookable'          : bookable,
+            'data-appointlet-email'             : data.email,
+            'data-appointlet-field-homephone'   : data.phone,
+            'data-appointlet-field-firstname'   : data.first_name,
+            'data-appointlet-field-lastname'    : data.last_name,
+            'data-appointlet-field-postalcode'  : data.zip,
+            'data-appointlet-field-program'     : data.program,
+            'data-appointlet-field-campuscode'  : data.campus,
+          });
+
         });
 
-      });
+      })
+      .fail(function( jqxhr, textStatus, error ) {
+        // handle any errors in request
+        // var err = textStatus + ", " + error;
+        // console.log( err );
+      })
+      .always( function() {
+        // run when done or failed
 
-      // load appointlet script when button has been updated
-      $.getScript( 'https://www.appointletcdn.com/loader/loader.min.js' );
+        // load appointlet script when button has been updated
+        $.getScript( 'https://www.appointletcdn.com/loader/loader.min.js' );
 
-    });
+      } );
+
 
   });
 
